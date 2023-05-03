1e297d8403121dfd0d9d782ccedb4a197312b58e
KShanmukhaSrinivas <shanmukha.kethanapalli@gmail.com>
2023-05-03 21:35:34 +0530

    6 commit

--- index.py
+++ index.py
@@ -2,9 +2,6 @@ from flask import Flask,render_template,redirect,request,url_for,session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "RWFSEGSRGTJNDSFABGE"
 @app.route('/success/<name>')
-//changed
-//second
-//first
 def success(name):
    return f'<h1>welcome {name}</h1>'
 
