7adfa5ec5e6445225a1d2ebd31bf4fa37f35356b
KShanmukhaSrinivas <shanmukha.kethanapalli@gmail.com>
2023-05-03 14:58:05 +0530

    Second commit

--- index.py
+++ index.py
@@ -1,15 +1,15 @@
-from flask import Flask,render_template,redirect,request,url_for
+from flask import Flask,render_template,redirect,request,url_for,session
 app = Flask(__name__)
-
-@app.route('/success')
+app.config['SECRET_KEY'] = "RWFSEGSRGTJNDSFABGE"
+@app.route('/success/<name>')
 def success(name):
-   return 'welcome %s' % name
+   return f'<h1>welcome {name}</h1>'
 
 @app.route('/attendance', methods = ['POST', 'GET'])
 def hello_world():
    if request.method == 'POST':
-      user = request.form
-      return redirect(url_for('success',name = user))
+      session['user'] = request.form["name"]
+      return redirect(url_for('success',name = session['user']))
    else:
       return render_template("attendance.html")
 
@@ -18,4 +18,4 @@ def hello_flask():
    return 'Hello Flask'
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug = True)
